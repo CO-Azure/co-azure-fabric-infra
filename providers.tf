81a91aa6c9486447309e691222980c2f95dbcfe3
Alex Lyon <alex.lyon@cabinetoffice.gov.uk>
2025-08-18 12:27:36 +0100

    base file structure

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  backend "azurerm" {}
+}
+
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
